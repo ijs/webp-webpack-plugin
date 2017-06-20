c428603d5e0440aa6c6991561b9485ecdaa02de1
Jerret321 <321jiangtao@gmail.com>
2017-06-21 00:09:41 +0800

    feature: make webp format file

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * reference: 
+ * https://github.com/lcxfs1991/blog/issues/1
+ * https://webpack.github.io/docs/how-to-write-a-plugin.html
+ */
+
+const defaultOpts = {
+    match: /\.(png|jpe?g)$/
+}
+function WebPWebpackPlugin(opts) {
+    this.opts = Object.assign({}, defaultOpts, opts)
+}
+WebPWebpackPlugin.prototype.apply = function (compiler) {
+    compiler.plugin('emit', (compilation, next) => {
+        console.log(compilation.assets)
+        Object.keys(compilation.assets)
+            .filter(assetPath => this.opts.match.test(assetPath))
+            .map(assetPath => compilation.assets[`${assetPath}.webp`] = compilation.assets[assetPath])
+
+        next()
+    })
+}
+
+module.exports = WebPWebpackPlugin
